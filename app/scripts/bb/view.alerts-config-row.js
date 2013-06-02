7fc1eb5b919ef91d8efb30d79c5372db9d2dd623
Anatoliy Zaslavskiy <adz@nycitt.com>
2013-06-02 11:51:52 -0400

    Alert Config Sort of Working

--- app/scripts/bb/view.alerts-config-row.js
+++ app/scripts/bb/view.alerts-config-row.js
@@ -12,7 +12,11 @@ function(
     return Backbone.Layout.extend({
         template: '#alerts-config-row',
         initialize: function(){
-
+            this.render();
+        },
+        render: function(){
+            console.log(this.template);
+            this.$el.html(this.template(this.model.toJSON()));
         }
     });
 });
--- app/scripts/bb/view.alerts-config.js
+++ app/scripts/bb/view.alerts-config.js
@@ -16,22 +16,28 @@ function(
     return Backbone.Layout.extend({
         template: '#alerts-config',
         config: new cAlertsConfig(),
+        capitaliseFirstLetter: function(string) {
+		    return string.charAt(0).toUpperCase() + string.slice(1);
+		},
         initialize: function(){
         	var metrics = ['dislike', 'elaborate', 'like', 'question'];
 
         	var config = this.config;
 
+        	var cap = this.capitaliseFirstLetter;
+
         	_(metrics).each(function(metric){
         		config.add({
         			metric: metric,
+        			metric_label: cap(metric),
         			live_responses: true,
         			alert_threshold: 3
         		});
         	});
 
-        	this.config.on('all', this.render, this);
+  			this.afterRender();
         },
-       	render: function(){
+       	afterRender: function(){
        		var $tbody = this.$('tbody');
 
        		$tbody.empty();
